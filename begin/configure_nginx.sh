7f3563d9d5a39143adf0ab6ec8d5bcb1c3921c82
carlosemv <vieiramecarlos@gmail.com>
2017-12-06 10:31:42 -0500

    clean

--- begin/configure_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install nginx
-apt-get update
-apt-get install nginx
-
-# set configuration file for getcast
-nginx/nginx_settings.sh getcast.nargothrond.me /home/getcast/public
--- begin/pgsql/pgsql_createuser.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# create user with db creation privileges
-sudo -u postgres bash -c "psql -c \"CREATE USER getcast WITH PASSWORD 'marvelouspandaband' CREATEDB;\""
